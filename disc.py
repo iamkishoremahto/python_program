749a66411f154333f01225320eaa59d644cfd72b
Kishore <kishorekumar.mahto@vmokshagroup.com>
2022-04-28 14:37:32 +0530

    dics and driveing

---
+++ disc.py
@@ -0,0 +1,12 @@
+word_meaning={"Mouse":"https://www.javatpoint.com/input-devices#Mouse",
+
+ "Computer":""" computer is a programmable electronic device that accepts raw data as input and 
+ processes it with a set of instructions (a program) to produce the result as output. It renders 
+ output just after performing mathematical and logical operations and can save the output for future use.
+  It can process numerical as well as non-numerical calculations. The term "computer" is derived from the
+   Latin word "computare" which means to calculate."""
+   
+   }
+   
+user = input('Enter the : ')
+print(word_meaning[user])
---
+++ driving_permision.py
@@ -0,0 +1,7 @@
+age=int(input('Enter your age : '))
+if(age>18):
+    print("You can drive")
+elif(age==18):
+    print("Come to the office for verification ")
+else:
+    print("you cannot drive")
