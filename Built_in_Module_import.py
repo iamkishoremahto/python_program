6f18879cf418cb2a42e48dadabc15ca45af9747b
Kishore <kishorekumar.mahto@vmokshagroup.com>
2022-05-05 18:25:17 +0530

    add two files

---
+++ Built_in_Module_import.py
@@ -0,0 +1,7 @@
+import random
+
+print(random.randint(0,5))
+print(random.random()*50)
+
+list=("kishore","dhano","nitesh","ritu","shiwani","sita")
+print(random.choice(list))
---
+++ snake_water_gun.py
@@ -0,0 +1,50 @@
+import random
+
+
+
+
+computerWin=0
+userWin=0
+n=10
+while(n>0):
+    list=("Snake","Water","Gun")
+    user=input("Enter your choice (Snake, Water, Gun) : ")
+    computer=random.choice(list)
+    if(user=="Snake" and computer=="Water"):
+        print("You are winner !!!")
+        userWin=userWin+1
+    elif(user=="Water" and computer=="Gun"):
+        print("You are winner !!!")
+        userWin=userWin+1
+    elif(user=="Gun" and computer=="Snake"):
+        print("You are winner !!!")
+        userWin=userWin+1
+    elif(computer=="Snake" and user=="Water"):
+        print("computer is winner !!!")
+        computerWin=computerWin+1
+    elif(computer=="Water" and user=="Gun"):
+        print("computer is winner !!!")
+        computerWin=computerWin+1
+    elif(computer=="Gun" and user=="Snake"):
+        print("computer is winner !!!")
+        computerWin=computerWin+1
+    elif(computer==user):
+        print("Both choices are equal")
+    n=n-1
+
+if(userWin>computerWin):
+    print("You are winner !!!")
+    print("Total user win : ",userWin)
+    print("Total Computer win : ",computerWin)
+elif(userWin==computerWin):
+    print("Both are Winner !!!")
+    print("Total user win : ",userWin)
+    print("Total Computer win : ",computerWin)
+else:
+    print("computer is winner !!!")
+    print("You are lost !!! Try next time ")
+
+    print("Total user win : ",userWin)
+    print("Total Computer win : ",computerWin)
+
+    
